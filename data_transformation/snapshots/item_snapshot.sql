70384df70239af20a5eb78801a71f40aff6629ce
kishankumarachutha <kishankumarachutha@gmail.com>
2024-09-09 12:45:01 +0530

    logs updated

--- data_transformation/snapshots/item_snapshot.sql
+++ data_transformation/snapshots/item_snapshot.sql
@@ -1,19 +1,19 @@
--- -- snapshots/item_snapshot.sql
+-- snapshots/item_snapshot.sql
 
--- {% snapshot item_snapshot %}
---     {{
---         config(
---             target_schema='snapshots',  -- Schema where snapshot will be stored
---             unique_key='item_name',     -- Unique key for tracking changes
---             strategy='check',           -- Use 'check' strategy to monitor specific columns
---             check_cols=['brand_name', 'msrp_in_usd']  -- Columns to track for changes
---         )
---     }}
+{% snapshot item_snapshot %}
+    {{
+        config(
+            target_schema='snapshots',  -- Schema where snapshot will be stored
+            unique_key='item_name',     -- Unique key for tracking changes
+            strategy='check',           -- Use 'check' strategy to monitor specific columns
+            check_cols=['brand_name', 'msrp_in_usd']  -- Columns to track for changes
+        )
+    }}
 
---     SELECT 
---         item_name,
---         brand_name,
---         msrp_in_usd
---     FROM {{ ref('stg_items') }}
+    SELECT 
+        item_name,
+        brand_name,
+        msrp_in_usd
+    FROM {{ ref('stg_items') }}
 
--- {% endsnapshot %}
+{% endsnapshot %}
